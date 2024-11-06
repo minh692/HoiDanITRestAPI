c8cead8d33f2d41e47412710923648141baa9166
minh692 <nguyennhatminh06092002@gmail.com>
2024-11-06 19:52:43 +0700

    update

--- src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 import java.util.List;
 import vn.hoidanit.jobhunter.domain.Company;
-import vn.hoidanit.jobhunter.domain.dto.ResultPaginationDTO;
+import vn.hoidanit.jobhunter.domain.response.ResultPaginationDTO;
 import vn.hoidanit.jobhunter.repository.CompanyRepositoy;
 
 @Service
